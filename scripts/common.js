ed327ca08bd7e1c81ca2f88b3536d6d457b7d98e
Mironenkov <mironenkovr@gmail.com>
2016-07-15 18:54:05 +0300

    First add

--- scripts/common.js
+++ scripts/common.js
@@ -2,5 +2,20 @@
  * Created by GloryManU on 04.07.2016.
  */
 (function(){
-	
+    $("#mainSlider").lightSlider({
+        item: 1,
+        pager: false
+    });
+    $("#saleSlider").lightSlider({
+        item: 4,
+        controls: false,
+        slideMargin: 20,
+        pager: true
+    });
+    $("#noveltySlider").lightSlider({
+        item: 4,
+        controls: false,
+        slideMargin: 20,
+        pager: true
+    });
 })();
