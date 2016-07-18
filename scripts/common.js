f421484d8a3227aa865601dd8618a0ce50f13899
Mironenkov <mironenkovr@gmail.com>
2016-07-18 18:39:12 +0300

    add Card_item made of 70%

--- scripts/common.js
+++ scripts/common.js
@@ -18,4 +18,22 @@
         slideMargin: 20,
         pager: true
     });
+    $(".cardSlider").lightSlider({
+        gallery: true,
+        item: 1,
+        vertical: true,
+        verticalHeight: 666,
+        vThumbWidth: 176,
+        thumbItem: 3,
+        thumbMargin: 12,
+        slideMargin: 0
+    });
+    $(".reviews-textSlider").lightSlider({
+        item: 2,
+        vertical: true,
+        slideMargin: 0,
+        pager: false,
+        slideMove: 2,
+        verticalHeight: 300
+    });
 })();
